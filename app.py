5441e2be6aa14d17a75b07c276a2c4fe665f10b4
Inamul2 <70210601+Inamul2@users.noreply.github.com>
2022-10-23 20:45:18 +0530

    Add files via upload

--- app.py
+++ app.py
@@ -374,19 +374,19 @@ def validate_url(url):
     return url
 
 
-# def getFavicon(domain):
-#         page = requests.get(domain)
-#         soup = BeautifulSoup(page.text, 'html.parser')
-#         icon_link = soup.find("link", rel="shortcut icon")
-#         if icon_link is None:
-#             icon_link = soup.find("link", rel="icon")
-#         if icon_link is None:
-#             return [domain + '/favicon.ico']
-#         return [icon_link["href"]]
-#
+def getFavicon1(domain):
+        page = requests.get(domain)
+        soup = BeautifulSoup(page.text, 'html.parser')
+        icon_link = soup.find("link", rel="shortcut icon")
+        if icon_link is None:
+            icon_link = soup.find("link", rel="icon")
+        if icon_link is None:
+            return [domain + '/favicon.ico']
+        return [icon_link["href"]]
+
 
 def getFavicon(domain):
-    favicons = []
+    favicons = getFavicon1(domain)
     page = requests.get(domain)
     soup = BeautifulSoup(page.text, 'html.parser')
     icon_link = soup.find_all("link", rel="shortcut icon")
